43a7e5b091934ef749a21dd807036f66d7a65928
Pawan Poudel <pawan.poudel@gmail.com>
2014-08-14 21:23:41 -0400

    Register for app did finish launching notification
    PPLocationManager class is currently designed to
    instantiate itself when the app is launched. With this
    approach we can keep the code in app delegate relatively
    clean.

--- SignificantlyChanged/Location/PPLocationManager.m
+++ SignificantlyChanged/Location/PPLocationManager.m
@@ -7,7 +7,44 @@
 //
 
 #import "PPLocationManager.h"
+#import "PPLocationFetcher.h"
+#import "PPGeocoder.h"
 
 @implementation PPLocationManager
+
+#pragma mark - Initializers
+
++ (PPLocationFetcher *)locationFetcher {
+    PPLocationFetcher *locationFetcher = [[PPLocationFetcher alloc] init];
+    locationFetcher.geocoder = [[PPGeocoder alloc] init];
+    
+    return locationFetcher;
+}
+
++ (instancetype)sharedInstance {
+    static PPLocationManager *locationManager = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        locationManager = [[self alloc] init];
+        locationManager.locationFetcher = [self locationFetcher];
+    });
+    
+    return locationManager;
+}
+
+#pragma mark - App lifecycle
+
++ (void)load {
+    [[NSNotificationCenter defaultCenter] addObserver:[self sharedInstance]
+                                             selector:@selector(applicationDidFinishLaunching:)
+                                                 name:UIApplicationDidFinishLaunchingNotification
+                                               object:nil];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    // TODO: Find out if the app was launched due to significant location change.
+    // TODO: Check to see if the location background mode is enabled or not.
+}
 
 @end
