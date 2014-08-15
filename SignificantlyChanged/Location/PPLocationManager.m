d9a6b9e55b722a19332f002a383829f6805ad9e6
Pawan Poudel <pawan.poudel@gmail.com>
2014-08-14 21:06:18 -0400

    Define PPLocationManager's public interface
    PPLocationManager is a façade providing access to the
    location subsystem. It collects interfaces exposed by
    various classes that provide highly specific location related
    functionalities and exposes their behaviors via a high
    level interface.

---
+++ SignificantlyChanged/Location/PPLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  PPLocationManager.m
+//  SignificantlyChanged
+//
+//  Created by PAWAN POUDEL on 8/13/14.
+//  Copyright (c) 2014 Pawan Poudel. All rights reserved.
+//
+
+#import "PPLocationManager.h"
+
+@implementation PPLocationManager
+
+@end
