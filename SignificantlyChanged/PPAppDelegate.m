1f280cc324d6692c72da7988d86cc9c10a21ec91
Pawan Poudel <pawan.poudel@gmail.com>
2014-08-16 12:54:43 -0400

    Ask user to give permission to use location when app launches

--- SignificantlyChanged/PPAppDelegate.m
+++ SignificantlyChanged/PPAppDelegate.m
@@ -7,13 +7,34 @@
 //
 
 #import "PPAppDelegate.h"
+#import "PPObjectConfigurator.h"
+#import "PPLocationManager.h"
+
+@interface PPAppDelegate ()
+
+@property (nonatomic) PPLocationManager *locationManager;
+
+@end
 
 @implementation PPAppDelegate
+
+#pragma mark - Accessors
+
+- (PPLocationManager *)locationManager {
+    if (_locationManager == nil) {
+        _locationManager = [[PPObjectConfigurator sharedInstance] locationManager];
+    }
+    
+    return _locationManager;
+}
+
+#pragma mark - App lifecycle
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self.locationManager promptForLocationPermissions];
     return YES;
-}							
+}
 
 @end
